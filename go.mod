299f3a053bc5a87efbac186960fd9d6d8169514c
Mykhailo <mykhailo_vozniak@epam.com>
2020-09-02 15:05:58 +0300

    remove init

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module module-3
+module github.com/mykhailovozniak/module-3
 
 go 1.14
--- hello.go
+++ hello.go
@@ -1,1 +1,7 @@
 package module_3
+
+import "fmt"
+
+func hello()  {
+	fmt.Println("this filed added only for testing purposes")
+}
